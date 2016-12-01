9de8dee13fdec8b369da3d0d06e5e576bba56b72
Tanja Kaiser <tanjakkaiser@gmail.com>
2016-12-01 19:46:38 +0100

    simplified soListener

--- src/so_data/soListener.py
+++ src/so_data/soListener.py
@@ -14,19 +14,17 @@ class GradientSensor(SimpleTopicSensor):
     "PassThrough" because the sensor just forwards the received msg
     """
     def __init__(self, name, id='', topic=None, message_type=None,  initial_value=None, create_log = False):
-        super(GradientSensor, self).__init__(topic=topic, name = name, message_type = message_type, initial_value = initial_value, create_log=create_log)
-
+        # buffer to store and handle gradient data 
         self._buffer = soBuffer.SoBuffer(id=id)
 
-    def update(self, newValue):
-        '''
-        This method is to refresh the _value.
-        '''
-        self._latestValue = self._buffer.get_current_gradient(newValue)
+        super(GradientSensor, self).__init__(topic=topic, name = name, message_type = message_type, initial_value = initial_value, create_log=create_log)
+
 
     def subscription_callback(self, msg):
-        self.update(msg)
+        tmp = self._buffer.get_current_gradient(msg)
+        self.update(tmp)
         rospy.logdebug("%s received sensor message: %s of type %s", self._name, self._value, type(self._value))
         if self._iShouldCreateLog:
             self._logFile.write("{0:f}\t{1:f}\n".format(rospy.get_time(), self._value))
             self._logFile.flush()
+
