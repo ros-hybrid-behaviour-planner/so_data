87ae7f4bee6d423fe8b912eca569d40105664390
Tanja Kaiser <tanjakkaiser@gmail.com>
2016-12-14 11:24:59 +0100

    first attempt to integrate updating frequency for gradients

--- src/so_data/soBroadcaster.py
+++ src/so_data/soBroadcaster.py
@@ -10,11 +10,13 @@ class SoBroadcaster():
     '''
     This class is the broadcaster to send data from one robot to the data space 
     '''
-    def __init__(self):
+    def __init__(self, time=0):
         '''
         Constructor
         Creates publisher to broadcast data to soData as an soMessage 
         '''
+        self._previous = rospy.Time.now()
+        self._desired = time
         self._pub = rospy.Publisher('soData', soMessage, queue_size = 1)
 
 
@@ -22,7 +24,12 @@ class SoBroadcaster():
         '''
         sends data to soData topic
         '''
+
         if isinstance(message, soMessage):
-            self._pub.publish(message)
+            #to send data only in certain time frames (does not work perfectly) ToDO
+            if rospy.Time.now() - self._previous >= rospy.Duration(self._desired):
+                self._pub.publish(message)
+                self._previous = rospy.Time.now()
         else:
             rospy.loginfo("Wrong message type")
+
