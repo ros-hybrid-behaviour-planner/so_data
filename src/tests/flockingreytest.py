f4e617b9df04d2683fd8217660807c2a1f31c46c
Tanja Kaiser <tanjakkaiser@gmail.com>
2017-02-03 13:29:43 +0100

    update flockingrey unit test

--- src/tests/flockingreytest.py
+++ src/tests/flockingreytest.py
@@ -7,42 +7,29 @@ Module with unit test for flockingrey.py
 """
 
 import unittest
-from so_data.msg import SoMessage
 from geometry_msgs.msg import Vector3, Quaternion
 from so_data.flockingrey import *
 import rospy
-import so_data.gradientnode
+from so_data.so_data_examples.gradientnode import create_gradient
 
 
 class FlockingAITest(unittest.TestCase):
     def testCohesion(self):
-        agent = so_data.gradientnode.create_gradient(Vector3(1, 2, 3),
-                                                     q=Quaternion(),
-                                                     direction=Vector3(1, 0,
-                                                                       0),
-                                                     goal_radius=1.0)
+        agent = create_gradient(Vector3(1, 2, 3), q=Quaternion(),
+                                direction=Vector3(1, 0, 0), goal_radius=1.0)
         neighbors = []
-        neighbors.append(so_data.gradientnode.create_gradient(Vector3(1, 2, 3),
-                                                              q=Quaternion(
-                                                                  0.707, 0.707,
-                                                                  0, 0),
-                                                              direction=Vector3(
-                                                                  1, 0, 0),
-                                                              goal_radius=1.0))
-        neighbors.append(so_data.gradientnode.create_gradient(Vector3(2, 2, 5),
-                                                              q=Quaternion(
-                                                                  0.707, 0.707,
-                                                                  0, 0),
-                                                              direction=Vector3(
-                                                                  1, 0, 0),
-                                                              goal_radius=1.0))
-        neighbors.append(so_data.gradientnode.create_gradient(Vector3(1, 6, 3),
-                                                              q=Quaternion(
-                                                                  0.707, 0.707,
-                                                                  0, 0),
-                                                              direction=Vector3(
-                                                                  1, 0, 0),
-                                                              goal_radius=1.0))
+        neighbors.append(create_gradient(Vector3(1, 2, 3),
+                                         q=Quaternion(0.707, 0.707, 0, 0),
+                                         direction=Vector3(1, 0, 0),
+                                         goal_radius=1.0))
+        neighbors.append(create_gradient(Vector3(2, 2, 5),
+                                         q=Quaternion(0.707, 0.707, 0, 0),
+                                         direction=Vector3(1, 0, 0),
+                                         goal_radius=1.0))
+        neighbors.append(create_gradient(Vector3(1, 6, 3),
+                                         q=Quaternion(0.707, 0.707, 0, 0),
+                                         direction=Vector3(1, 0, 0),
+                                         goal_radius=1.0))
 
         result = cohesion(agent, neighbors)
         result.x = round(result.x, 2)
@@ -52,45 +39,30 @@ class FlockingAITest(unittest.TestCase):
         self.assertEqual(result, Vector3(0.33, 1.33, 0.67))
 
     def testAlignment(self):
-        agent = so_data.gradientnode.create_gradient(Vector3(1, 2, 3),
-                                                     q=Quaternion(),
-                                                     direction=Vector3(1, 0,
-                                                                       0),
-                                                     goal_radius=1.0)
+        agent = create_gradient(Vector3(1, 2, 3), q=Quaternion(),
+                                direction=Vector3(1, 0, 0), goal_radius=1.0)
         neighbors = []
-        neighbors.append(so_data.gradientnode.create_gradient(Vector3(1, 2, 3),
-                                                              q=Quaternion(
-                                                                  0.707, 0.707,
-                                                                  0, 0),
-                                                              direction=Vector3(
-                                                                  1, 0, 0),
-                                                              goal_radius=1.0))
+        neighbors.append(create_gradient(Vector3(1, 2, 3),
+                                         q=Quaternion(0.707, 0.707, 0, 0),
+                                         direction=Vector3(1, 0, 0),
+                                         goal_radius=1.0))
 
         result = alignment(agent, neighbors)
 
         self.assertEqual(result, Vector3(-1, 1, 0))
 
     def testSeparation(self):
-        agent = so_data.gradientnode.create_gradient(Vector3(1, 2, 3),
-                                                     q=Quaternion(),
-                                                     direction=Vector3(1, 0,
-                                                                       0),
-                                                     goal_radius=1.0)
+        agent = create_gradient(Vector3(1, 2, 3), q=Quaternion(),
+                                direction=Vector3(1, 0, 0), goal_radius=1.0)
         neighbors = []
-        neighbors.append(so_data.gradientnode.create_gradient(Vector3(2, 2, 5),
-                                                              q=Quaternion(
-                                                                  0.707, 0.707,
-                                                                  0, 0),
-                                                              direction=Vector3(
-                                                                  1, 0, 0),
-                                                              goal_radius=1.0))
-        neighbors.append(so_data.gradientnode.create_gradient(Vector3(1, 6, 3),
-                                                              q=Quaternion(
-                                                                  0.707, 0.707,
-                                                                  0, 0),
-                                                              direction=Vector3(
-                                                                  1, 0, 0),
-                                                              goal_radius=1.0))
+        neighbors.append(create_gradient(Vector3(2, 2, 5),
+                                         q=Quaternion(0.707, 0.707, 0, 0),
+                                         direction=Vector3(1, 0, 0),
+                                         goal_radius=1.0))
+        neighbors.append(create_gradient(Vector3(1, 6, 3),
+                                         q=Quaternion(0.707, 0.707, 0, 0),
+                                         direction=Vector3(1, 0, 0),
+                                         goal_radius=1.0))
 
         result = separation(agent, neighbors)
         result.x = round(result.x, 2)
